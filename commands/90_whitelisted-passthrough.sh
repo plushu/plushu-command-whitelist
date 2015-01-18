2c6b542af9d9b23dbfa1111cde28052e90d3e16b
Stuart P. Bentley <stuart@testtrack4.com>
2015-01-18 14:07:24 -0800

    Commands cleanup

--- commands/90_whitelisted-passthrough.sh
+++ commands/90_whitelisted-passthrough.sh
@@ -1,8 +1,11 @@
-#!/usr/bin/env bash
-set -eo pipefail; [[ -n "$PLUSHU_TRACE" ]] && set -x
+# If the whitelist exists
+if [[ -e "$PLUSHU_DIR/command.whitelist" ]] &&
+  # and this command is on the whitelist
+  grep -Fxq "$1" "$PLUSHU_DIR/command.whitelist"; then
 
-list="$PLUSHU_DIR/command.whitelist"
-
-if [[ -e "$list" ]] && grep -Fxq "$1" "$list"; then
+  # Execute the command
   "$@"
+
+  # Mark that we handled this command
+  export PLUSHU_COMMAND_HANDLED=$1
 fi
