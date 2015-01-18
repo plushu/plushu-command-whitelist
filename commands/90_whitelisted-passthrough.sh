d03193a27a77333c3bc3ba02cce7faf37ac2ff78
Stuart P. Bentley <stuart@testtrack4.com>
2015-01-18 02:18:40 -0800

    Fix commands

--- commands/90_whitelisted-passthrough.sh
+++ commands/90_whitelisted-passthrough.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 set -eo pipefail; [[ -n "$PLUSHU_TRACE" ]] && set -x
 
-shift
-
 list="$PLUSHU_DIR/command.whitelist"
 
-if [[ -e "$list" && grep -Fxq "$1" "$list" ]]; then
+if [[ -e "$list" ]] && grep -Fxq "$1" "$list"; then
   "$@"
 fi
